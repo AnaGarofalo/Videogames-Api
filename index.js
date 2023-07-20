ff0be89fa4c390f052ead649f0bf523e7256cc7e
anagarofalo <anamariagarofalo95@gmail.com>
2023-07-20 19:35:38 -0300

    fixed start error

--- index.js
+++ index.js
@@ -22,13 +22,9 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
-try {
-  // Syncing all the models at once.
-  conn.sync({ alter: true }).then(() => {
-    server.listen(PORT, () => {
-      console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
-    });
+// Syncing all the models at once.
+conn.sync({ alter: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
-} catch (error) {
-  console.log(error.message);
-}
+});
--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-const server = require("../index");
+const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+const server = express();
 
 require("./db.js");
 
