5a6138836156af9009fe74c686936e491b0053d8
anagarofalo <anamariagarofalo95@gmail.com>
2023-07-20 18:30:06 -0300

    render

--- index.js
+++ index.js
@@ -22,9 +22,13 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
-// Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+try {
+  // Syncing all the models at once.
+  conn.sync({ alter: true }).then(() => {
+    server.listen(PORT, () => {
+      console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    });
   });
-});
+} catch (error) {
+  console.log(error.message);
+}
